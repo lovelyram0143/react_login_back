371a7934b70da832b6c7d94e82739f64692ff1af
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2024-04-20 10:25:50 +0530

    connect sql to node

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react_login_back
+
+npm init
+
+npm install express mssql
+npm install msnodesqlv8 --save
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const sql = require("mssql/msnodesqlv8");
+
+const app = express();
+
+// SQL Server configuration
+var config = {
+    
+    server: "(localdb)\\MSSQLLocalDB  ", // Server IP address
+    database: "Demodatabase", // Database name
+    options: {
+        encrypt: false, // Disable encryption
+        trustedConnection: true,
+        enableArithAbort: true,
+        trustServerCertificate: true,
+    }
+}
+
+// Connect to SQL Server
+sql.connect(config, err => {
+    if (err) {
+        console.log(err)
+        throw err;
+    }
+    console.log("Connection Successful!");
+});
+
+// Define route for fetching data from SQL Server
+app.get("/", (request, response) => {
+    // Execute a SELECT query
+    new sql.Request().query("SELECT * FROM students", (err, result) => {
+        if (err) {
+            console.error("Error executing query:", err);
+        } else {
+            response.send(result.recordset); // Send query result as response
+            console.dir(result.recordset);
+        }
+    });
+});
+
+// Start the server on port 3000
+app.listen(3000, () => {
+    console.log("Listening on port 3000...");
+});
